e14c6664416fd5970a4f561f269b386842c08da2
Minh Lộc <83216771+minhloc29@users.noreply.github.com>
2025-02-16 10:09:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ def favicon():
     return send_from_directory('static',
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
